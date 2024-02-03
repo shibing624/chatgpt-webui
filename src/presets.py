5a0d575d14921cfbacbdbc7fb5dec238c34d4b9c
shibing624 <shibing624@126.com>
2024-02-03 23:29:57 +0800

    enable web search cite.

--- src/presets.py
+++ src/presets.py
@@ -130,7 +130,7 @@ Web search results:
 {web_results}
 Current date: {current_date}
 
-Instructions: Using the provided web search results, write a comprehensive reply to the given query. Make sure to cite results using [[number](URL)] notation after the reference. If the provided search results refer to multiple subjects with the same name, write separate answers for each subject.
+Instructions: Using the provided web search results, write a comprehensive reply to the given query. Make sure to cite results using [citation:x] notation after the reference, where x is a number. If the provided search results refer to multiple subjects with the same name, write separate answers for each subject.
 Query: {query}
 Reply in {reply_language}
 """
